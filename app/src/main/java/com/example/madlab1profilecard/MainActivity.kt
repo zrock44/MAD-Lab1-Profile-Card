b01844fedb7d5bfd687f28095dacc3c8fd0275d0
zrock44 <zrock44@tutanota.com>
2025-01-31 19:26:22 -0500

    Added image resource and image composable function

--- app/src/main/java/com/example/madlab1profilecard/MainActivity.kt
+++ app/src/main/java/com/example/madlab1profilecard/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.madlab1profilecard.ui.theme.MADLab1ProfileCardTheme
 
@@ -19,29 +22,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MADLab1ProfileCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    ProfileImage()
                 }
             }
         }
     }
 }
 
+//Image for the user profile. In this case it's my cat Biggie
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ProfileImage(modifier: Modifier = Modifier) {
+    val profileImage = painterResource(R.drawable.profile_image)
+    Image(
+        contentDescription = stringResource(R.string.profile_image_description),
+        painter = profileImage,
+        modifier = Modifier.clip(CircleShape)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ProfileCardPreview() {
     MADLab1ProfileCardTheme {
-        Greeting("Android")
+        ProfileImage()
     }
 }
